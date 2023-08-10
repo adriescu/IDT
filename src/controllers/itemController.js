4711cd3a00234e1112e283f012775249a744f72c
adriescu <95098450+adriescu@users.noreply.github.com>
2023-08-09 21:42:11 -0300

    eliminada linea innecesaria

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,8 +1,4 @@
 const itemController = {
-    showItemList: function(req, res){
-        res.send(`Mostrando lista de todos los items`);
-    },
-
     showItem: function(req, res){
         res.send(`Enviando item ${req.params.id}`);
     },
