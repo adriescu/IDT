e174cc6dd62d5282d4debe4436f710f2a19a20b1
adriescu <95098450+adriescu@users.noreply.github.com>
2023-09-25 23:10:06 -0300

    lista e index

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/modules/views")
